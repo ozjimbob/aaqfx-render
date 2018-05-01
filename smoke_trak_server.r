e4e15b482959e86296b614e4babcc57f22e5a91d
Grant Williamson <Grant.Williamson@utas.edu.au>
2018-05-01 11:04:23 +1000

    Disaggregation fix

--- smoke_trak_server.r
+++ smoke_trak_server.r
@@ -398,7 +398,7 @@ gen_poly = function(algorithm,distance=25,defaultAngle=15,defaultAccuracy=10,poi
     # Calculate a kernel density raster with a 3km bandwidth
     sm = smooth_map(line_is,bandwidth=2,to.Raster=TRUE,nrow=50,ncol=50)
     rast = sm[[1]]
-    rastr = disaggregate(rast,4,method="bilinear")
+    rast = disaggregate(rast,4,method="bilinear")
     # Plot this raster on the map of Melbourne
     #tm_shape(comb,is.master = TRUE) + tm_lines(alpha=0) + tm_shape(rast) + tm_raster(palette="YlOrRd",title="KDE") + tm_shape(vic) + tm_borders()  + tm_shape(roads,col="black",alpha=0.5) + tm_lines() + tm_layout(title = "Intersection Kernel Density")
     
